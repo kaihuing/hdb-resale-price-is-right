402e1eb656c5ccc86ca2d393fbdb1591b2f0ff14
kaihuing <51510075+kaihuing@users.noreply.github.com>
2019-12-17 23:03:34 +0800

    Add files via upload

---
+++ The (Resale) Price is Right/0. Preliminary Tests/Geocoding with R (Tamp Pilot).R
@@ -0,0 +1,21 @@
+library(readr)
+library(ggmap)
+register_google(key="AIzaSyBgczf5a61gv-drXmko1tA9Wi53HJuqtn8", write=TRUE)
+
+df <- read_csv("resale_tamp_18.csv")
+head(df)
+
+get_lon_lat <- function(x) {
+  geocode(x)
+}
+
+library(dplyr)
+
+lon_lat <- get_lon_lat(df$address)
+
+df2 <- merge(df, lon_lat, by="row.names", quiet=TRUE)
+
+glimpse(df2)
+head(df2)
+
+write.csv(df2, "tamp_18_lonlat.csv")
